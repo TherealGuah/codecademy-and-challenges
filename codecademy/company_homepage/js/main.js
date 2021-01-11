7b1d6e1d5208a08425380df7ff74fcb8c7c4d877
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2021-01-11 20:34:57 +0000

    main.js next button working in company_homepage

--- codecademy/company_homepage/js/main.js
+++ codecademy/company_homepage/js/main.js
@@ -1,7 +1,6 @@
 // array to store images to display in loop
 let employeeImage = [
     "./resources/images/employees/Michaella.jpg",
-    "./resources/images/employees/Pietra.jpg",
     "./resources/images/employees/Sandra.jpg",
     "./resources/images/employees/Tadeusz.jpg",
     "./resources/images/employees/Tamara.jpg",
@@ -11,18 +10,42 @@ let employeeImage = [
 // array to store figcaptions to match the images
 let figCaption = [
     "Michaella is responsible for our reception.",
-    "Pietra is a vonlunteers every weekend for walks.",
     "Sandra is our founder.",
     "Tadeusz  is our in-house vet.",
     "Tamara is a volunteer during the week.",
     "Ricardo is also one of our founders."
 ];
 
-// create a function that changes the picture to the next in the array
+// image object to target its src attribute
+const empImage = document.getElementById('empImg');
 
-const changePicture = () => {
-    
-
-
+// var to establish thge index number of the picture to use
+let index = 0;
+// function that changes the picture to the next in the array
+const changePictureNext = () => {
+    empImage.setAttribute("src", employeeImage[index]);
+    index++;
+    if (index >= employeeImage.length) {
+        index = 0;
+    }
 }
 
+// function that changes the picture to the previous in the array
+const changePicturePrevious = () => {
+    if (index === 0 ) {
+        index = employeeImage.length - 1;
+        empImage.setAttribute('src', employeeImage[index]);
+        index--;
+    } else {
+        empImage.setAttribute('src', )
+    }
+    
+}
+
+// Store Previous and Next elements into Variables
+const nextElement = document.getElementById('right');
+const previousElement = document.getElementById('left');
+
+// event listeners for both elements
+nextElement.addEventListener('click', changePictureNext);
+previousElement.addEventListener('click',changePicturePrevious);
