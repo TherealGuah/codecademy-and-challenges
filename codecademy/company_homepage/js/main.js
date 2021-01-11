5f2cd9efe99eb18bd4b5b2f7be4d1be79d5a8d60
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2021-01-11 17:43:54 +0000

    main.js for company_homepage variable arrays initialized

--- codecademy/company_homepage/js/main.js
+++ codecademy/company_homepage/js/main.js
@@ -1,2 +1,28 @@
-let employees = [];
+// array to store images to display in loop
+let employeeImage = [
+    "./resources/images/employees/Michaella.jpg",
+    "./resources/images/employees/Pietra.jpg",
+    "./resources/images/employees/Sandra.jpg",
+    "./resources/images/employees/Tadeusz.jpg",
+    "./resources/images/employees/Tamara.jpg",
+    "./resources/images/employees/Ricardo.jpg"
+];
+
+// array to store figcaptions to match the images
+let figCaption = [
+    "Michaella is responsible for our reception.",
+    "Pietra is a vonlunteers every weekend for walks.",
+    "Sandra is our founder.",
+    "Tadeusz  is our in-house vet.",
+    "Tamara is a volunteer during the week.",
+    "Ricardo is also one of our founders."
+];
+
+// create a function that changes the picture to the next in the array
+
+const changePicture = () => {
+    
+
+
+}
 
