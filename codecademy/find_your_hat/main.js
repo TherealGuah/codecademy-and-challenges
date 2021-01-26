1d11c2929bb24f57f5d8a56f899e28cf9abfdc40
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2021-01-26 17:33:41 +0000

    .print() added to Field class

--- codecademy/find_your_hat/main.js
+++ codecademy/find_your_hat/main.js
@@ -6,5 +6,28 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-    
+    constructor(field) {
+        this._field = field;
+    } 
+    // getter method
+    get field() {
+        return this._field;
+    }
+// to print out
+    print() {
+        this._field.forEach(x => {
+            console.log(x.join(''))
+        });   
+    }
 }
+
+// testing
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+  
+  //console.log(myField.field);
+  //myField.print();
+
